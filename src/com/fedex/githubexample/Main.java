25cb4f211b706615cf4118da55d15dfa78942b77
883368 <883368@CFSIT883368LT5.corp.ds.fedex.com>
2017-11-27 09:55:18 -0700

    Initial Checkin of GitHubExample

---
+++ src/com/fedex/githubexample/Main.java
@@ -0,0 +1,12 @@
+package com.fedex.githubexample;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		displayMessage();
+	}
+	
+	private static void displayMessage() {
+		System.out.println("Welcome to my GitHub!");
+	}
+}
