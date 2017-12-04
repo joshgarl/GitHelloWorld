f624e7f687ebd203116327cd402ebfa5098f904c
883368 <883368@CFSIT883368LT5.corp.ds.fedex.com>
2017-12-04 13:33:06 -0700

    testing merge vs rebase

--- src/com/fedex/githubexample/Main.java
+++ src/com/fedex/githubexample/Main.java
@@ -11,6 +11,8 @@ public class Main {
 		System.out.println("Welcome Josh's GitHub!");
 	
 		System.out.println("Test2");
+		
+		System.out.println("testing merge vs rebase");
 	}
 	
 	private static void displayMessage2() {
